4980fdd416511a508041afd6811503c9e9f93235
Jack Nam <jack@expensify.com>
2023-08-23 18:04:55 -0700

    Add eslint for common _ to indicate unused

--- src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
+++ src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
@@ -1,8 +1,5 @@
 import {GetPermissionDeniedHelpLink, DEFAULT_APP_SETTINGS_LINK} from './getPermissionDeniedHelpLink.types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (_helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
-    return settingsLink;
-};
+const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (_helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => settingsLink;
 
 export default getPermissionDeniedHelpLink;
--- src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.web.tsx
+++ src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.web.tsx
@@ -1,8 +1,5 @@
 import {GetPermissionDeniedHelpLink, DEFAULT_APP_SETTINGS_LINK} from './getPermissionDeniedHelpLink.types';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, _settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
-    return helpLink;
-};
+const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, _settingsLink = DEFAULT_APP_SETTINGS_LINK) => helpLink;
 
 export default getPermissionDeniedHelpLink;
