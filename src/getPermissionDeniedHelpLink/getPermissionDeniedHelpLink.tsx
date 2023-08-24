bfe242e74e2e27637ff36a7b7ac722e65a2aa271
Jack Nam <jack@expensify.com>
2023-08-23 18:00:26 -0700

    Another potential prettier fix

--- src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
+++ src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
@@ -1,7 +1,7 @@
 import {GetPermissionDeniedHelpLink, DEFAULT_APP_SETTINGS_LINK} from './getPermissionDeniedHelpLink.types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
+const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (_helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
     return settingsLink;
 };
 
--- src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.web.tsx
+++ src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.web.tsx
@@ -1,7 +1,7 @@
 import {GetPermissionDeniedHelpLink, DEFAULT_APP_SETTINGS_LINK} from './getPermissionDeniedHelpLink.types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
+const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, _settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
     return helpLink;
 };
 
