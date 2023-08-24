cfa1899a01b48f960de9186311f6ac3e2e0c132a
Jack Nam <jack@expensify.com>
2023-08-23 17:52:34 -0700

    add eslint disabling

--- src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
+++ src/getPermissionDeniedHelpLink/getPermissionDeniedHelpLink.tsx
@@ -1,6 +1,7 @@
 import {GetPermissionDeniedHelpLink, DEFAULT_APP_SETTINGS_LINK} from './getPermissionDeniedHelpLink.types';
 
-const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (_helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const getPermissionDeniedHelpLink: GetPermissionDeniedHelpLink = (helpLink, settingsLink = DEFAULT_APP_SETTINGS_LINK) => {
     return settingsLink;
 };
 
